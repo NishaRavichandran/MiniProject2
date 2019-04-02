59ebf5d519a664ea2865e7c506c5038c45311d33
NishaRavichandran <runisha93@gmail.com>
2019-04-02 12:34:08 -0400

    Unit Test : Inserted a new car in Cars Table

---
+++ tests/Unit/InsertCarTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Car;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InsertCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCarTest()
+    {
+            $car = new Car();
+            $car->user_id='51';
+            $car->make='Honda';
+            $car->model='Sedan';
+            $car->year='1988';
+            $this->assertTrue($car->save());
+
+    }
+}
