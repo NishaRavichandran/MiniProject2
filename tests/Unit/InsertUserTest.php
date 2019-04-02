19f0c85ee64906184796ff5b7855b6629290e0b2
NishaRavichandran <runisha93@gmail.com>
2019-04-02 10:54:37 -0400

    Unit Test 5: Inserted an User to Users Table

---
+++ tests/Unit/InsertUserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InsertUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertUserTest()
+    {
+        $user = new User();
+
+        $user->name='Sunil';
+
+        $user->email='sn438@example.com';
+
+        $user->password='RtyUiOoXop';
+
+        $this->assertTrue($user->save());
+
+    }
+}
