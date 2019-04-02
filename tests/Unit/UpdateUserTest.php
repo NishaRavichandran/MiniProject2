868bae51d0568baabb484c79352440151ffc616d
NishaRavichandran <runisha93@gmail.com>
2019-04-02 11:31:14 -0400

    Unit Test 6: Updated the name of an User to Steve Smith in Users Table

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUserTest()
+    {
+
+        $user = User::find(5);
+
+        $user->name = 'Steve Smith';
+
+        $this->assertTrue($user->save());
+
+    }
+
+}
